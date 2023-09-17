fbcc0a4fc92dff7309986e9b5c7ce2da855a51b4
AmirShamsi02 <110440897+AmirShamsi02@users.noreply.github.com>
2023-09-17 12:56:20 +0330

    login-signup

--- account/views.py
+++ account/views.py
@@ -1,15 +1,43 @@
-from django.shortcuts import render
-import requests
-# Create your views here.
-def login(request):
-    if request.method == 'POST':
-        username = request.POST.get('username', False)
-        password = request.POST.get('password', False)
 
-        inputs_val = [username, password]
+from django.views.generic import View
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login,logout
+from .models import User
 
-        if User.objects.filter(username=username, password=password).exists():
-            user = User.objects.get(username=username, password=password)
-            request.session['user_id'] = user.id
-            
+
+class Login(View):
+
+    def get(self,request):
+        return render(request,'account/login.html')
+
+    def post(self,request):
+        post = request.POST
+        phonenumber = post["phonenumber"]
+        password = post["password"]
+
+        user = authenticate(request, phonenumber=phonenumber, password=password)
+        if user is not None:
+            login(request, user)
+            #return redirect()
+
+
+class Signup(View):
+    def get(self,request):
+        return render(request,'account/signup.html')
     
+    def post(self, request):
+        post = request.POST
+        firstname = post["firstname"]
+        lastname = post["lastname"]
+        phonenumber = post["phonenumber"]
+        password = post["password"]
+        password2 = post["password2"]
+
+        if User.objects.filter(phonenumber=phonenumber).exists():
+            #eror
+            pass
+        if password == password2:
+            new_user = User(first_name=firstname,last_name=lastname,phonenumber=phonenumber)
+            new_user.set_password(password)
+            new_user.save()
+            return redirect('account:login')
