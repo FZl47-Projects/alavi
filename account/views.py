d87b2674c166e8b5c68ba23afabf38557d8f95ba
AmirShamsi02 <110440897+AmirShamsi02@users.noreply.github.com>
2023-09-17 11:34:32 +0330

    login temp

--- account/views.py
+++ account/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
-
+import requests
 # Create your views here.
+def login(request):
+    if request.method == 'POST':
+        username = request.POST.get('username', False)
+        password = request.POST.get('password', False)
+
+        inputs_val = [username, password]
+
+        if User.objects.filter(username=username, password=password).exists():
+            user = User.objects.get(username=username, password=password)
+            request.session['user_id'] = user.id
+            
+    
