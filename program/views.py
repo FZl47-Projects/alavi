d98431baf865abf49e6610d69e66bd885bb9de6b
AmirShamsi02 <110440897+AmirShamsi02@users.noreply.github.com>
2023-09-20 12:29:30 +0330

    .

--- program/views.py
+++ program/views.py
@@ -1,17 +1,20 @@
 from django.shortcuts import render
 from django.views.generic import View
+from django.contrib.auth.mixins import LoginRequiredMixin
 from requests import request
 from account.models import User
 from program.models import Days
-from django.contrib.auth.models import User 
+from program.models import Training_program_object
+from program.models import Diet_program_object
+
 
 # Create your views here.
 
-class Exercies(View):
+class Exercies(LoginRequiredMixin,View):
     template_name = 'program/exercise.html'
     def get(self,request):
         user = request.user
-        program = user.days.exercise_program_objects
+        program = user.days.training_program_objects
         context = {
             'user' : user,
             'program' : program
