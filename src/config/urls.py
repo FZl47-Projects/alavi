ff356b10ecd4805c31b5bfde73014bfad5bf9a06
AmirMahdi Azarboon <amirazarboon@gmail.com>
2023-10-20 12:26:28 +0330

    Change admin/login url to login_registe view (conf/urls)

--- src/config/urls.py
+++ src/config/urls.py
@@ -3,8 +3,11 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
+from account.views import login_register
+
 
 urlpatterns = [
+    path('admin/login/', login_register, name='login_register'),
     path('admin/', admin.site.urls),
     path('u/', include('account.urls', namespace='account')),
     path('', include('public.urls', namespace='public')),
