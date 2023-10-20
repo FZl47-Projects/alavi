c9754d517dc2ec5f1c65d8ade59a8016eb8dd29f
AmirMahdi Azarboon <amirazarboon@gmail.com>
2023-10-20 11:28:02 +0330

    Modify core/admin (Change admin site header/title, Remove django-q & group models)

--- src/core/admin.py
+++ src/core/admin.py
@@ -1,3 +1,22 @@
+from django.utils.translation import gettext_lazy as _
+from django.contrib.auth.models import Group
 from django.contrib import admin
 
-# Register your models here.
+from django_q.models import Failure, OrmQ, Schedule, Success
+
+
+# Unregister groups
+admin.site.unregister(Group)
+
+
+# Unregister django-q models
+admin.site.unregister(Failure)
+admin.site.unregister(OrmQ)
+admin.site.unregister(Schedule)
+admin.site.unregister(Success)
+
+
+# Rename admin site header & title & index_title
+admin.site.site_header = _("Alavi sport club")
+admin.site.index_title = _("Management panel")
+admin.site.site_title = _("Alavi gym")
