34bac6fac5c7b2f8fe5cdcd5a8f5275ff9a91481
AmirMahdi Azarboon <amirazarboon@gmail.com>
2023-10-21 21:39:56 +0330

    Add verbose name to exercise app

--- src/exercise/apps.py
+++ src/exercise/apps.py
@@ -1,6 +1,8 @@
+from django.utils.translation import gettext_lazy as _
 from django.apps import AppConfig
 
 
 class ExerciseConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'exercise'
+    verbose_name = _('Exercise')
