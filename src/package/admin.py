184481445de02e3e608157ac7518929aff5f4e88
AmirMahdi Azarboon <amirazarboon@gmail.com>
2023-10-22 21:57:23 +0330

    Add custom PackageAdmin

--- src/package/admin.py
+++ src/package/admin.py
@@ -1,3 +1,24 @@
+from django.utils.translation import gettext as _
+from django.db import models as a_model
 from django.contrib import admin
+from django import forms
+from .models import Package
 
-# Register your models here.
+
+# Package Model Admin
+@admin.register(Package)
+class PackageAdmin(admin.ModelAdmin):
+    list_display = ('type', 'intcomma_price')
+    list_display_links = ('type',)
+
+    # Change formField attributes(size)
+    formfield_overrides = {
+        a_model.IntegerField: {"widget": forms.NumberInput(attrs={"size": "18"})},
+    }
+
+    def intcomma_price(self, obj):
+        """ Return thousand seperated price. """
+        if obj.price:
+            return "{:,}".format(obj.price)
+
+    intcomma_price.short_description = _('Price (Rials)')
