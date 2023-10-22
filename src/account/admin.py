3da31012626e2049441c8f6db1fec8a0b0846940
AmirMahdi Azarboon <amirazarboon@gmail.com>
2023-10-22 19:58:03 +0330

    Modify CustomUserAdmin(override some methods)

--- src/account/admin.py
+++ src/account/admin.py
@@ -1,30 +1,46 @@
-from django.contrib import admin
+from django.utils.translation import gettext as _
 from django.contrib.auth.admin import UserAdmin
+from django.contrib import admin
 from .forms import CustomUserCreationForm, CustomUserChangeForm
 from .models import User
 
+
+@admin.register(User)
 class CustomUserAdmin(UserAdmin):
     add_form = CustomUserCreationForm
     form = CustomUserChangeForm
 
     model = User
 
-    list_display = ('phonenumber','email', 'is_active',
-                    'is_staff', 'is_superuser', 'last_login','role')
-    list_filter = ('is_active', 'is_staff', 'is_superuser','role','first_name','last_name')
+    list_display = ('get_raw_phonenumber', 'email', 'first_name', 'last_name', 'role', 'is_active', 'is_staff')
+    list_filter = ('is_active', 'is_staff', 'is_superuser', 'role')
+    search_fields = ('email', 'phonenumber')
+    ordering = ('email', 'phonenumber')
     fieldsets = (
-        (None, {'fields': ('phonenumber','email', 'password','role','first_name','last_name')}),
-        ('Permissions', {'fields': ('is_staff', 'is_active',
-         'is_superuser', 'groups', 'user_permissions')}),
-        ('Dates', {'fields': ('last_login', 'date_joined')})
+        (None, {'fields': ('phonenumber', 'email', 'first_name', 'last_name', 'role')}),
+        (_('Permissions'), {'fields': ('is_staff', 'is_active', 'is_superuser', 'user_permissions')}),
+        (_('Dates'), {'fields': ('last_login', 'date_joined')})
     )
     add_fieldsets = (
         (None, {
             'classes': ('wide',),
-            'fields': ('phonenumber','email', 'password1', 'password2', 'is_staff', 'is_active','first_name','last_name')}
+            'fields': ('phonenumber', 'email', 'first_name', 'last_name', 'password1', 'password2', 'is_staff', 'is_active')}
          ),
     )
-    search_fields = ('email','phonenumber')
-    ordering = ('email','phonenumber')
+    
+    def has_change_permission(self, request, obj=None):
+        """ Prevent staff users from changing super_users obj. """
+        if obj is not None and request.user.is_staff and obj.is_superuser:
+            return False
+        
+        return super().has_change_permission(request, obj)
 
-admin.site.register(User, CustomUserAdmin)
+    def get_readonly_fields(self, request, obj=None):
+        """ Prevent staff users to change some fields. """
+        if request.user.is_superuser:
+            return []
+
+        fields = obj.get_model_fields()
+        excludes = ['first_name', 'last_name']
+
+        return [field.name for field in fields if field.name not in excludes]
