eb727866be28a3f419e6b7dd1c871f73263c67e0
AmirMahdi Azarboon <amirazarboon@gmail.com>
2023-10-23 11:23:29 +0330

    Modify Certificate model

--- src/public/models.py
+++ src/public/models.py
@@ -1,7 +1,12 @@
+from django.utils.translation import gettext as _
 from django.db import models
 
 
+# Certificates model
 class Certificate(models.Model):
-    picture = models.ImageField(upload_to='images/certificates')
+    title = models.CharField(_('Certificate title'), max_length=64, null=True, blank=True, help_text=_('Optional'))
+    picture = models.ImageField(_('Picture'), upload_to='images/certificates')
 
-
+    class Meta:
+        verbose_name = _('Certificate')
+        verbose_name_plural = _('Certificates')
