2382de89c501b4cf763b8b1ad6784b28e0b8f61e
AmirMahdi Azarboon <amirazarboon@gmail.com>
2023-12-06 12:45:23 +0330

    Add new package

--- apps/package/enums.py
+++ apps/package/enums.py
@@ -9,5 +9,6 @@ class PackageChoices(TextChoices):
     CROSSFIT = 'crossfit', _('Crossfit')
     WORKOUT_DIET = 'workout_diet', _('Workout + Diet')
     CROSSFIT_DIET = 'crossfit_diet', _('Crossfit + Diet')
+    PRIVATE_WORKOUT_DIET = 'private_workout_diet', _('Private workout + Diet')
     ANY_DIET = 'any_diet', _('Any sport + Diet')
     OFFLINE = 'offline', _('Offline')
